71ff95076c197012f7c78c4aaf8da402a125a4c2
Johan Talboom <borfje@gmail.com>
2015-03-19 17:49:33 +0100

    ProjectieDemo now shows the frustum in an external cameraview

--- week1.projectie/ProjectieDemo.cpp
+++ week1.projectie/ProjectieDemo.cpp
@@ -17,7 +17,13 @@ int main(int argc, char* argv[])
 
 
 
-ProjectieDemo::ProjectieDemo()
+ProjectieDemo::ProjectieDemo() : 
+	topleft(glm::vec3(0, 0, 0), glm::vec3(0, 0, 0)), 
+	bottomright(glm::vec3(0, 0, 0), glm::vec3(0, 0, 0)), 
+	topright(glm::vec3(0, 0, 0), glm::vec3(0, 0, 0)), 
+	bottomleft(glm::vec3(0, 0, 0), glm::vec3(0, 0, 0)),
+	center(glm::vec3(0, 0, 0), glm::vec3(0, 0, 0))
+
 {
 	appSetup.renderer = blib::AppSetup::GlRenderer;
 	appSetup.border = false;
@@ -42,10 +48,13 @@ void ProjectieDemo::init()
 	shader->setUniformName(Uniforms::modelMatrix, "modelMatrix", blib::Shader::Mat4);
 	shader->finishUniformSetup();
 	renderer->renderState.activeShader = shader;
+	renderer->renderState.depthTest = true;
 
 	rot = 0;
 	ortho = false;
 	fov = 90.0f;
+	nearPlane = 0.1f;
+	farPlane = 7.5f;
 }
 
 void ProjectieDemo::update(double elapsedTime)
@@ -64,6 +73,21 @@ void ProjectieDemo::update(double elapsedTime)
 
 	rot += 90 * (float)elapsedTime;
 
+
+	glm::mat4 cam = glm::lookAt(glm::vec3(1.75f, 2.25f, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 proj;
+	if (ortho)
+		proj = glm::ortho(-5.0f, 5.0f, -5.0f, 5.0f, -50.0f, 50.0f);
+	else
+		proj = glm::perspective(fov, 640 / 720.0f, 0.1f, 100.0f);
+
+
+	renderer->setViewPort(0, 0, 1280, 720);
+	renderer->unproject(glm::vec2(0, 0), NULL, &topleft, cam, proj);
+	renderer->unproject(glm::vec2(1280, 0), NULL, &topright, cam, proj);
+	renderer->unproject(glm::vec2(0, 720), NULL, &bottomleft, cam, proj);
+	renderer->unproject(glm::vec2(1280, 720), NULL, &bottomright, cam, proj);
+	renderer->unproject(glm::vec2(1280/2, 720/2), NULL, &center, cam, proj);
 	prevKeyState = keyState;
 }
 
@@ -99,6 +123,14 @@ void ProjectieDemo::draw()
 	boxVerts.push_back(blib::VertexP3C4(glm::vec3(s, -s, s), color));
 	boxVerts.push_back(blib::VertexP3C4(glm::vec3(-s, -s, s), color));
 
+	std::vector<blib::VertexP3C4> axis;
+	axis.push_back(blib::VertexP3C4(glm::vec3(-1000, 0, 0), glm::vec4(1, 0, 0, 1)));
+	axis.push_back(blib::VertexP3C4(glm::vec3(1000, 0, 0), glm::vec4(1, 0, 0, 1)));
+	axis.push_back(blib::VertexP3C4(glm::vec3(0, -1000, 0), glm::vec4(0, 1, 0, 1)));
+	axis.push_back(blib::VertexP3C4(glm::vec3(0, 1000, 0), glm::vec4(0, 1, 0, 1)));
+	axis.push_back(blib::VertexP3C4(glm::vec3(0, 0, -1000), glm::vec4(0, 0, 1, 1)));
+	axis.push_back(blib::VertexP3C4(glm::vec3(0, 0, 1000), glm::vec4(0, 0, 1, 1)));
+
 
 
 
@@ -108,17 +140,67 @@ void ProjectieDemo::draw()
 	if (ortho)
 		shader->setUniform(Uniforms::projectionMatrix, glm::ortho(-5.0f, 5.0f, -5.0f, 5.0f, -50.0f, 50.0f));
 	else
-		shader->setUniform(Uniforms::projectionMatrix, glm::perspective(fov, 640 / 720.0f, 0.1f, 100.0f));
+		shader->setUniform(Uniforms::projectionMatrix, glm::perspective(fov, 640 / 720.0f, nearPlane, farPlane));
 	shader->setUniform(Uniforms::cameraMatrix, glm::lookAt(glm::vec3(1.75f, 2.25f, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0)));
-	shader->setUniform(Uniforms::modelMatrix, glm::rotate(glm::mat4(), rot, glm::vec3(0,1,0)));
+	shader->setUniform(Uniforms::modelMatrix, glm::rotate(glm::mat4(), rot, glm::vec3(0, 1, 0)));
 	renderer->drawLines(boxVerts, 10.0f);
+	shader->setUniform(Uniforms::modelMatrix, glm::mat4());
+	renderer->drawLines(axis, 1.0f);
+
 
 
 	renderer->setViewPort(640, 0, 640, 720);
 	shader->setUniform(Uniforms::projectionMatrix, glm::perspective(70.0f, 640 / 720.0f, 0.1f, 100.0f));
-	shader->setUniform(Uniforms::cameraMatrix, glm::lookAt(glm::vec3(3.75f, 4.25f, 4), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0)));
+	shader->setUniform(Uniforms::cameraMatrix, glm::lookAt(glm::vec3(3.75f, 4.25f, 4), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0)) * glm::rotate(glm::mat4(), rot*0.1f, glm::vec3(0, 1, 0)));
 	shader->setUniform(Uniforms::modelMatrix, glm::rotate(glm::mat4(), rot, glm::vec3(0, 1, 0)));
 	renderer->drawLines(boxVerts, 10.0f);
+
+
+	std::vector<blib::VertexP3C4> frustumVerts;
+	color.r = 1;
+
+	float f = farPlane;
+	float n = nearPlane;
+	if (ortho)
+	{
+		f = 50;
+		n = -50;
+	}
+
+	frustumVerts.push_back(blib::VertexP3C4(topleft.origin + topleft.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(topright.origin + topright.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(topright.origin + topright.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(bottomright.origin + bottomright.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(bottomright.origin + bottomright.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(bottomleft.origin + bottomleft.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(bottomleft.origin + bottomleft.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(topleft.origin + topleft.dir * n, color));
+
+	frustumVerts.push_back(blib::VertexP3C4(topleft.origin + topleft.dir * f, color));
+	frustumVerts.push_back(blib::VertexP3C4(topright.origin + topright.dir * f, color));
+	frustumVerts.push_back(blib::VertexP3C4(topright.origin + topright.dir * f, color));
+	frustumVerts.push_back(blib::VertexP3C4(bottomright.origin + bottomright.dir * f, color));
+	frustumVerts.push_back(blib::VertexP3C4(bottomright.origin + bottomright.dir * f, color));
+	frustumVerts.push_back(blib::VertexP3C4(bottomleft.origin + bottomleft.dir * f, color));
+	frustumVerts.push_back(blib::VertexP3C4(bottomleft.origin + bottomleft.dir * f, color));
+	frustumVerts.push_back(blib::VertexP3C4(topleft.origin + topleft.dir * f, color));
+
+
+	frustumVerts.push_back(blib::VertexP3C4(topleft.origin + topleft.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(topleft.origin + topleft.dir * f, color));
+
+	frustumVerts.push_back(blib::VertexP3C4(topright.origin + topright.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(topright.origin + topright.dir * f, color));
+
+	frustumVerts.push_back(blib::VertexP3C4(bottomleft.origin + bottomleft.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(bottomleft.origin + bottomleft.dir * f, color));
+
+	frustumVerts.push_back(blib::VertexP3C4(bottomright.origin + bottomright.dir * n, color));
+	frustumVerts.push_back(blib::VertexP3C4(bottomright.origin + bottomright.dir * f, color));
+
+	shader->setUniform(Uniforms::modelMatrix, glm::mat4());
+	renderer->drawLines(axis, 1.0f);
+	renderer->drawLines(frustumVerts, 7.0f);
 
 
 
--- week1.projectie/ProjectieDemo.h
+++ week1.projectie/ProjectieDemo.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <blib/App.h>
+#include <blib/math/Ray.h>
 
 namespace blib { class Shader; }
 
@@ -17,8 +18,18 @@ class ProjectieDemo : public blib::App
 
 	bool ortho;
 	float fov;
+	float nearPlane;
+	float farPlane;
 
 	blib::KeyState prevKeyState;
+	
+	blib::math::Ray topleft;
+	blib::math::Ray topright;
+	blib::math::Ray bottomleft;
+	blib::math::Ray bottomright;
+	blib::math::Ray center;
+
+
 public:
 	ProjectieDemo();
 	~ProjectieDemo();
